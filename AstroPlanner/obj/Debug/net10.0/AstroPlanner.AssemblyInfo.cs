7515c5352272267891107205ed828c799a2a6a23
Nathanael Hartmann <tankhardrive@yahoo.com>
2026-04-13 22:09:06 -0400

    Update icon

--- AstroPlanner/obj/Debug/net10.0/AstroPlanner.AssemblyInfo.cs
+++ AstroPlanner/obj/Debug/net10.0/AstroPlanner.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AstroPlanner")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b042b2a52154a4ca2fa41a45217b2bfc758eda9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf49b5db45252935f96d284adf875e8e58378444")]
 [assembly: System.Reflection.AssemblyProductAttribute("AstroPlanner")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AstroPlanner")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
